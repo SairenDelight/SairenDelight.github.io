acd5733bd192fd1477f20d8d0c3df4ade4a06c73
Manavivian <h.vivian92@gmail.com>
2018-07-04 17:02:56 -0700

    Changed the layout

---
+++ scripts/main_script.js
@@ -0,0 +1,7 @@
+function openSocialMedia() {
+  let parent= document.getElementById('btn-container');
+  let gmailIcon = document.createElement("a");
+  gmailIcon.setAttribute("href","mailto:vivianhoang0@gmail.com");
+  let linkedinIcon = document.createElement("a");
+  parent.append(gmailIcon);
+}
