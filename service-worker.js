11aba6ce0d47386db7ccc85cb59c40407753c277
Vivian Hoang <vivianhoang0@gmail.com>
2019-05-27 09:35:10 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.b8db5659bd6639408ae3bcaa995cbe81.js"
 );
 
 self.addEventListener('message', (event) => {
